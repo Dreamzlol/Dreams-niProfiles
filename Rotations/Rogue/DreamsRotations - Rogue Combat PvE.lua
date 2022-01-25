7183658b15f20c87d269da4ba4bd9afe1e920ab5
Dreamzlol <denis.dasilvarocha@icloud.com>
2022-01-26 00:27:00 +0100

    Improved rogue rotation

--- Rotations/Rogue/DreamsRotations - Rogue Combat PvE.lua
+++ Rotations/Rogue/DreamsRotations - Rogue Combat PvE.lua
@@ -1,6 +1,6 @@
 --------------------------------
 -- DreamsRotation Rogue - Combat PvE
--- Version - 1.0.4
+-- Version - 1.0.5
 -- Author - Dreams
 --------------------------------
 -- Changelog
@@ -9,6 +9,7 @@
 -- 1.0.2 Added Tricks of the Trade on Focus Target
 -- 1.0.3 Added Auto Target
 -- 1.0.4 Added GUI
+-- 1.0.5 Improved Rotation
 --------------------------------
 local ni = ...
 
@@ -30,22 +31,21 @@ local items = {
         text = "\124T" .. select(3, GetSpellInfo(2764)) .. ":26:26\124t Use Auto Target",
         tooltip = "Use the Auto Target feature if you in combat it will Auto Target the closest enemy around you",
         enabled = true,
-        key = "getSetting_AutoTarget",
+        key = "autotarget",
     },
     {
         type = "entry",
         text = "\124T" .. select(3, GetSpellInfo(57934)) .. ":26:26\124t Use Tricks of the Trade on Focus Target",
         tooltip = "Use Tricks of the Trade on your focus target be sure too have your focus on main tank or on a other rogue",
         enabled = true,
-        key = "getSetting_TricksOfTheTrade",
+        key = "tricksofthetrade",
     },
     {
         type = "entry",
-        text = "\124T" .. select(3, GetSpellInfo(54758)) ..
-            ":26:26\124t Use Engineer Glove Enchant (Hyperspeed Accelerators)",
+        text = "\124T" .. select(3, GetSpellInfo(54758)) .. ":26:26\124t Use Engineer Glove Enchant (Hyperspeed Accelerators)",
         tooltip = "Use Engineer Glove Enchant if your target is a boss",
         enabled = true,
-        key = "getSetting_HyperspeedAccelerators",
+        key = "hyperspeedaccelerators",
     },
     {
         type = "separator",
@@ -59,7 +59,7 @@ local items = {
         text = "\124T" .. select(3, GetSpellInfo(48638)) .. ":26:26\124t Use Sinister Strike",
         tooltip = "Use Sinister Strike as combopoint builder",
         enabled = true,
-        key = "getSetting_SinisterStrike",
+        key = "sinisterstrike",
     },
     {
         type = "entry",
@@ -67,7 +67,7 @@ local items = {
         tooltip = "Use Slice and Dice if you have the amount or more than the defined combopoints",
         enabled = true,
         value = 2,
-        key = "getSetting_SliceAndDice",
+        key = "sliceanddice",
     },
     {
         type = "entry",
@@ -75,7 +75,7 @@ local items = {
         tooltip = "Use Rupture if you have the amount or more than the defined combopoints",
         enabled = true,
         value = 2,
-        key = "getSetting_Rupture",
+        key = "rupture",
     },
     {
         type = "entry",
@@ -83,13 +83,13 @@ local items = {
         tooltip = "Use Eviscerate if you have the amount of the defined combopoints",
         enabled = true,
         value = 5,
-        key = "getSetting_Eviscerate",
+        key = "eviscerate",
     },
     {
         type = "entry",
         text = "\124T" .. select(3, GetSpellInfo(51690)) .. ":26:26\124t Use Killing Spree",
         tooltip = "Use Killing Spree when your target is a boss",
-        key = "getSetting_KillingSpree",
+        key = "killingspree",
         enabled = true,
     },
     {
@@ -97,14 +97,14 @@ local items = {
         text = "\124T" .. select(3, GetSpellInfo(13877)) .. ":26:26\124t Use Blade Flurry",
         tooltip = "Use Blade Flurry when your target is a boss",
         enabled = true,
-        key = "getSetting_BladeFlurry",
+        key = "bladeflurry",
     },
     {
         type = "entry",
         text = "\124T" .. select(3, GetSpellInfo(13750)) .. ":26:26\124t Use Adrenaline Rush",
         tooltip = "Use Adrenaline Rush when your target is a boss",
         enabled = true,
-        key = "getSetting_AdrenalineRush",
+        key = "adrenalinerush",
     },
 }
 
@@ -126,6 +126,21 @@ local function onunload()
     ni.GUI.DestroyFrame("DreamsRotations - Rogue Combat PvE");
 end
 
+-- Spells
+local StartAttack = GetSpellInfo(6603)
+local TricksOfTheTrade = GetSpellInfo(57934)
+local KillingSpree = GetSpellInfo(51690)
+local BladeFlurry = GetSpellInfo(13877)
+local AdrenalineRush = GetSpellInfo(13750)
+local SliceAndDice = GetSpellInfo(6774)
+local Rupture = GetSpellInfo(48672)
+local Eviscerate = GetSpellInfo(48668)
+local SinisterStrike = GetSpellInfo(48638)
+
+-- Items
+local Food = GetSpellInfo(45548)
+local Drink = GetSpellInfo(57073)
+
 local queue = {
     "Pause Rotation",
     "Auto Target",
@@ -144,14 +159,21 @@ local queue = {
 local abilities = {
     ["Pause Rotation"] = function()
         if IsMounted()
+        or UnitIsDeadOrGhost("player")
+        or UnitIsDeadOrGhost("target")
+        or UnitUsingVehicle("player")
+        or UnitInVehicle("player")
         or not UnitAffectingCombat("player")
-        or UnitIsDeadOrGhost("player") then
+        or ni.unit.ischanneling("player")
+        or ni.unit.iscasting("player")
+        or ni.unit.buff("player", Food)
+        or ni.unit.buff("player", Drink) then
             return true;
         end
     end,
 
     ["Auto Target"] = function()
-        local _, enabled = GetSetting("getSetting_AutoTarget")
+        local _, enabled = GetSetting("autotarget")
         if enabled then
             if UnitAffectingCombat("player")
             and ((ni.unit.exists("target")
@@ -169,26 +191,27 @@ local abilities = {
         and UnitCanAttack("player", "target")
         and not UnitIsDeadOrGhost("target")
         and UnitAffectingCombat("player")
-        and not IsCurrentSpell(6603) then
-            ni.spell.cast(6603)
+        and not IsCurrentSpell(StartAttack) then
+            ni.spell.cast(StartAttack)
             return true;
         end
     end,
 
     ["Tricks of the Trade"] = function()
-        local _, enabled = GetSetting("getSetting_TricksOfTheTrade")
+        local _, enabled = GetSetting("tricksofthetrade")
         if enabled then
-            if ni.spell.valid("focus", "57934", false, true, true)
-            and ni.spell.available("Tricks of the Trade")
-            and ni.unit.exists("focus") and ni.player.power() < 85 then
-                ni.spell.cast("Tricks of the Trade", "focus")
+            if ni.spell.available(TricksOfTheTrade)
+            and ni.spell.valid("focus", TricksOfTheTrade, true, true)
+            and ni.unit.exists("focus")
+            and ni.player.power() < 85 then
+                ni.spell.cast(TricksOfTheTrade, "focus")
                 return true;
             end
         end
     end,
 
     ["Hyperspeed Accelerators"] = function()
-        local _, enabled = GetSetting("getSetting_HyperspeedAccelerators")
+        local _, enabled = GetSetting("hyperspeedaccelerators")
         if enabled then
             if ni.unit.isboss("target")
             and ni.player.slotcd(10) == 0 then
@@ -199,86 +222,89 @@ local abilities = {
     end,
 
     ["Killing Spree"] = function()
-        local _, enabled = GetSetting("getSetting_KillingSpree")
+        local _, enabled = GetSetting("killingspree")
         if enabled then
-            if ni.spell.available("Killing Spree")
+            if ni.spell.available(KillingSpree)
             and ni.unit.isboss("target")
-            and ni.unit.debuff("target", "Rupture", "player")
-            and ni.unit.buff("player", "Slice and Dice")
+            and ni.unit.debuff("target", Rupture, "player")
+            and ni.unit.buff("player", SliceAndDice)
             and ni.player.power() < 60 then
-                ni.spell.cast("Killing Spree", "target")
+                ni.spell.cast(KillingSpree, "target")
                 return true;
             end
         end
     end,
 
     ["Blade Flurry"] = function()
-        local _, enabled = GetSetting("getSetting_BladeFlurry")
+        local _, enabled = GetSetting("bladeflurry")
         if enabled then
-            if ni.spell.available("Blade Flurry")
+            if ni.spell.available(BladeFlurry)
             and ni.unit.isboss("target")
-            and ni.unit.debuff("target", "Rupture", "player")
-            and ni.unit.buff("player", "Slice and Dice") then
-                ni.spell.cast("Blade Flurry", "target")
+            and ni.unit.debuff("target", Rupture, "player")
+            and ni.unit.buff("player", SliceAndDice) then
+                ni.spell.cast(BladeFlurry)
                 return true;
             end
         end
     end,
 
     ["Adrenaline Rush"] = function()
-        local _, enabled = GetSetting("getSetting_AdrenalineRush")
+        local _, enabled = GetSetting("adrenalinerush")
         if enabled then
-            if ni.spell.available("Adrenaline Rush")
+            if ni.spell.available(AdrenalineRush)
             and ni.unit.isboss("target")
-            and ni.unit.debuff("target", "Rupture", "player")
-            and ni.unit.buff("player", "Slice and Dice")
+            and ni.unit.debuff("target", Rupture, "player")
+            and ni.unit.buff("player", SliceAndDice)
             and ni.player.power() < 40 then
-                ni.spell.cast("Adrenaline Rush", "target")
+                ni.spell.cast(AdrenalineRush)
                 return true;
             end
         end
     end,
 
     ["Slice and Dice"] = function()
-        local value, enabled = GetSetting("getSetting_SliceAndDice")
+        local value, enabled = GetSetting("sliceanddice")
         if enabled then
-            if ni.spell.available("Slice and Dice")
+            if ni.spell.available(SliceAndDice)
             and GetComboPoints("player", "target") >= value
-            and ni.unit.buffremaining("player", "Slice and Dice", "player") <= 2 then
-                ni.spell.cast("Slice and Dice", "target")
+            and ni.unit.buffremaining("player", SliceAndDice, "player") <= 2 then
+                ni.spell.cast(SliceAndDice)
                 return true;
             end
         end
     end,
 
     ["Rupture"] = function()
-        local value, enabled = GetSetting("getSetting_Rupture")
+        local value, enabled = GetSetting("rupture")
         if enabled then
-            if ni.spell.available("Rupture")
+            if ni.spell.available(Rupture)
+            and ni.spell.valid("target", Rupture, true, true)
             and GetComboPoints("player", "target") >= value
-            and ni.unit.debuffremaining("target", "Rupture", "player") <= 2 then
-                ni.spell.cast("Rupture", "target")
+            and ni.unit.debuffremaining("target", Rupture, "player") <= 2 then
+                ni.spell.cast(Rupture, "target")
                 return true;
             end
         end
     end,
 
     ["Eviscerate"] = function()
-        local value, enabled = GetSetting("getSetting_Eviscerate")
+        local value, enabled = GetSetting("eviscerate")
         if enabled then
-            if ni.spell.available("Eviscerate")
+            if ni.spell.available(Eviscerate)
+            and ni.spell.valid("target", Eviscerate, true, true)
             and GetComboPoints("player", "target") == value then
-                ni.spell.cast("Eviscerate", "target")
+                ni.spell.cast(Eviscerate, "target")
                 return true;
             end
         end
     end,
 
     ["Sinister Strike"] = function()
-        local _, enabled = GetSetting("getSetting_SinisterStrike")
+        local _, enabled = GetSetting("sinisterstrike")
         if enabled then
-            if ni.spell.available("Sinister Strike") then
-                ni.spell.cast("Sinister Strike", "target")
+            if ni.spell.available(SinisterStrike)
+            and ni.spell.valid("target", SinisterStrike, true, true) then
+                ni.spell.cast(SinisterStrike, "target")
                 return true;
             end
         end
